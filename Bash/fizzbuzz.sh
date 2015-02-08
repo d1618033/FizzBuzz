8fc1ceceed5ff8e59f372030aca42cee16d5c082
d1618033 <d1618033@gmail.com>
2015-02-08 21:31:26 +0200

    removed unnecessary function

--- Bash/fizzbuzz.sh
+++ Bash/fizzbuzz.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-function divides()
-{
-    if [ -eq `echo $1 / $2 * $2 | bc` $1 ] ; then
-        return 1
-    else
-        return 0
-    fi
-}
-
 n=$1
 for i in `seq 1 $n`; do
     if  [ $(( $i % 15 )) -eq 0 ] ; then
